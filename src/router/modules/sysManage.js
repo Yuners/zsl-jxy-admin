05753369b3904be1aa7d09266163de13574c5e5f
项海冬 <1298394207@qq.com>
2020-12-28 16:18:05 +0800

    [添加] 添加系统管理

--- src/router/modules/sysManage.js
+++ src/router/modules/sysManage.js
@@ -5,20 +5,20 @@ const sysManageRouter = {
     path: '/sysManage',
     name: 'sysManage',
     component: TopLayout,
-    redirect: '/jinSwim/home',
+    redirect: '/sysManage/home',
     meta: {title: '系统管理'},
     children: [
       {
-        path: 'home',
+        path: '/sysManage/home',
         name: 'Home',
         component: Layout,
-        redirect: '/jinSwim/home/index',
+        redirect: '/sysManage/home/index',
         meta: {title: 'Home', icon: 'dashboard'},
         children: [
           {
             path: 'index',
             name: 'Index',
-            component: () => import('@/views/jinSwim/Home/index'),
+            component: () => import('@/views/sysManage/Home/index'),
             hidden: true
           }
         ]

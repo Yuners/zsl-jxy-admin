ce280b567afd344cff77ed02cc0d56e387694ca4
fengrenren <785482254@qq.com>
2021-01-16 18:45:45 +0800

    【修改】api接口

--- src/api/dictionary.js
+++ src/api/dictionary.js
@@ -1,12 +1,12 @@
 import request from '@/utils/request'
 
-let index="http://192.124.1.57:5021/jxy"
+let index="/jxy"
 /**
  * 根据PCode查询字典，包括子集
  * @param params
  * @returns {AxiosPromise<any>}
  */
 export function getDictionaryAllByPCode( params ) {
-    
+
     return request.post(index+'/dictionary/getDictionaryAllByPCode', params)
 }
